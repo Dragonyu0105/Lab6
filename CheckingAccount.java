21391ca83545e86bacea3152f859340e159e9c87
Dragonyu0105 <90631466+Dragonyu0105@users.noreply.github.com>
2021-11-18 22:18:39 -0500

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,42 @@
+/**
+ * This is the checking account for the Lab 6
+ * @author winstonyu
+ *
+ */
+public class CheckingAccount extends BankAccount{
+	
+	/*
+	 * This is the static constant fee
+	 */
+	static final double FEE = 0.15;
+	
+	/**
+	 * This is a constructor for the checkings account
+	 * @param accountName
+	 * @param inAmount
+	 */
+	public CheckingAccount(String accountName, int inAmount) {
+		
+		//Takes parameters from the BankAccount
+		super(accountName, inAmount);
+		
+		//This sets the account number
+		this.setAccountNumber(getAccountNumber() + "-10");
+		
+		
+	}
+	
+	/**
+	 * This is a method to show whether the money you withdrawn
+	 * is more or less than the money you have in your account
+	 */
+	@Override
+	public boolean withdraw(double amount) {
+		
+		//This is to hold the amount of money withdrawn plus the fee
+		double withdraw = amount + FEE;
+		
+		//This returns the variable within a method
+		return super.withdraw(withdraw);
+	}
+}
